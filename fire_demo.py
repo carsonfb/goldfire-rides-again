b74d55cd90d4c0bc831b98032068b3790d1888ff
Carson F. Ball <carson.ball@gmail.com>
2021-11-11 14:10:12 -0600

    Colors are displaying now and the palette functions are stubbed in.

--- fire_demo.py
+++ fire_demo.py
@@ -1,17 +1,27 @@
 import OpenGL.GL as gl
 import OpenGL.GLUT as glut
 #from OpenGL.GLU import *
+import numpy
 
 # https://www.youtube.com/watch?v=a4NVQC_2S2U
 
 WINDOW_W = 640
 WINDOW_H = 480
 
-def showScreen():
+def read_palettes():
+    pass
+
+def make_palette():
+    pass
+
+def display_frame():
+    pixels = bytes([0xFF, 0x7F, 0x00, 0xFF, 0x00, 0xFF, 0X7F, 0XFF])
+    bitmap = numpy.fromstring(pixels * 320 * 480, numpy.uint8)
+
     gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
     gl.glLoadIdentity()
+    gl.glDrawPixels(WINDOW_W, WINDOW_H, gl.GL_RGBA, gl.GL_FLOAT, bitmap)
     glut.glutSwapBuffers()
-
 
 def main():
     glut.glutInit()
@@ -21,11 +31,11 @@ def main():
 
     glut.glutInitDisplayMode(glut.GLUT_RGBA)
     glut.glutInitWindowSize(WINDOW_W, WINDOW_H)
-    glut.glutInitWindowPosition(int((screen_w - WINDOW_W) / 2), int((screen_h - WINDOW_H) / 2));
+    glut.glutInitWindowPosition(int((screen_w - WINDOW_W) / 2), int((screen_h - WINDOW_H) / 2))
     wind = glut.glutCreateWindow("GoldFire Rides Again")
-    glut.glutDisplayFunc(showScreen)
-    glut.glutIdleFunc(showScreen)
+    glut.glutDisplayFunc(display_frame)
+    glut.glutIdleFunc(display_frame)
     glut.glutMainLoop()
 
 if __name__ == "__main__":
-        main()
+    main()
